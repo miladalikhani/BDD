28b63ecaec9cdd13bbe0333c54dcd91d13f69645
m.chraghalikhani <miladalikhani76@gmail.com>
2021-07-04 15:06:17 +0430

    Initial commit

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package calculator;
+
+public class Calculator {
+    public int add(int val1, int val2) {
+        return val1 + val2;
+    }
+}
---
+++ src/test/java/calculator/MyStepdefs.java
@@ -0,0 +1,38 @@
+package calculator;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class MyStepdefs {
+    private Calculator calculator;
+    private int val1;
+    private int val2;
+    private int result;
+
+
+    @Before
+    public void before()
+    {
+        calculator = new Calculator();
+    }
+
+    @Given("^Two input values, (\\d+) and (\\d+)$")
+    public void twoInputValuesAnd(int arg0, int arg1) {
+        val1 = arg0;
+        val2 = arg1;
+    }
+
+    @When("^I add the two values$")
+    public void iAddTheTwoValues() {
+        result = this.calculator.add(val1, val2);
+        System.out.println(result);
+    }
+
+    @Then("^I expect the result (\\d+)$")
+    public void iExpectTheResult(int arg0) {
+        Assert.assertEquals(arg0, result);
+    }
+}
